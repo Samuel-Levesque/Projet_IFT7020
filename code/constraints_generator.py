d432ec15a91e85f2fbdfc119caba7003bad5f672
Samuel Lévesque <samuel-levesque@hotmail.com>
2020-03-28 13:48:14 -0400

    Completed teams time restrictions generator

--- code/constraints_generator.py
+++ code/constraints_generator.py
@@ -16,7 +16,8 @@ class ConstraintsGenerator:
 
     def generate_scenario(self, divisions_size,
                           n_shared_coaches, game_length,
-                          venue_n_courts, date_start):
+                          venue_n_courts, date_start,
+                          n_time_restrictions, n_venue_restrictions):
         self.game_length = game_length
         self.divisions_size = divisions_size
         self.number_of_games = self._calculate_n_games(divisions_size)
@@ -27,6 +28,9 @@ class ConstraintsGenerator:
                 date_start,
                 game_length,
                 self.number_of_games)
+
+        self.teams_time_restrictions = self._generate_time_restrictions(n_time_restrictions)
+        self.teams_venue_restrictions = self._generate_venue_restrictions(n_venue_restrictions)
 
     def _calculate_n_games(self, divisions_size):
         n_games = 0
@@ -85,19 +89,19 @@ class ConstraintsGenerator:
                 last_end = last_start
                 while last_start < latest_start and last_end < latest_start:
                     last_start = random_between_two_times(
-                            last_start,
+                            last_end,
                             latest_start,
                             rounding_method="floor")
                     last_end = random_between_two_times(
-                            last_start + datetime.timedelta(minutes=game_length),
+                            last_start + datetime.timedelta(minutes=game_length+5),
                             eod,
                             rounding_method="ceiling")
                     index += 1
                     availabilities.loc[index, "Venue"] = available_venue["Venue"]
                     availabilities.loc[index, "Court"] = available_venue["Court"]
-                    availabilities.loc[index, "Date"] = date.strftime("%Y-%m-%d")
-                    availabilities.loc[index, "Time Start"] = last_start.strftime("%Hh%M")
-                    availabilities.loc[index, "Time End"] = last_end.strftime("%Hh%M")
+                    availabilities.loc[index, "Date"] = date#.strftime("%Y-%m-%d")
+                    availabilities.loc[index, "Time Start"] = last_start#.strftime("%Hh%M")
+                    availabilities.loc[index, "Time End"] = last_end#.strftime("%Hh%M")
                     available_game_spots += ((last_end - last_start).seconds/60.0) // game_length
                     if available_game_spots >= n_games:
                         break
@@ -106,6 +110,76 @@ class ConstraintsGenerator:
 
             date += datetime.timedelta(days=1)
         return availabilities
+
+    def _generate_time_restrictions(self, n_restrictions):
+        """
+        Restrictions are defined using the following format:
+            Team: Team name for which we want to apply constraint
+            Date: Date for which we want to apply the constraint
+            Constraint Type: Before/After. If before, the team prefers NOT to play before
+                specified time. If after, the team preferes NOT to play after specified time
+            Constraint Time: Time threshold used to define the constraint.
+        """
+        time_restrictions = pandas.DataFrame(columns=["Team", "Date", "Constraint Type", "Constraint Time"])
+        teams = self.teams["Team Name"].unique().tolist()
+        dates = self.court_availabilities["Date"].unique().tolist()
+#        dates = [datetime.datetime.strptime(date, "%Y-%m-%d") for date in dates]
+        applied_constraints = {}
+        n_applied_constraints = 0
+        while n_applied_constraints < n_restrictions:
+            chosen_team = random.choice(teams)
+            chosen_date = random.choice(dates)
+            lookup_key = (chosen_team, chosen_date)
+            earliest_time = chosen_date.replace(hour=8, minute=0)
+            latest_time = chosen_date.replace(hour=23, minute=0)
+            # Look if constraint was already applied on this team/date
+            if lookup_key in applied_constraints:
+                previous_constraints = applied_constraints[(chosen_team, chosen_date)]
+                if len(previous_constraints) > 1:
+                    # No more than 2 constraints per team/date
+                    continue
+                if previous_constraints[0][0] == "before":
+                    constraint_type = ["after"]
+                    constraint_time = random_between_two_times(
+                            earliest_time,
+                            previous_constraints[0][1],
+                            rounding_method="ceiling")
+                elif previous_constraints[0][0] == "after":
+                    constraint_type = ["before"]
+                    constraint_time = random_between_two_times(
+                            previous_constraints[0][1],
+                            latest_time,
+                            rounding_method="floor")
+            else:
+                constraint_type = random.choice(["before", "after"])
+                if constraint_type == "before":
+                    constraint_time = random_between_two_times(
+                            earliest_time,
+                            latest_time,
+                            "ceiling")
+                elif constraint_type == "after":
+                    constraint_time = random_between_two_times(
+                            earliest_time,
+                            latest_time,
+                            "floor")
+
+            time_restrictions.loc[n_applied_constraints, "Team"] = chosen_team
+            time_restrictions.loc[n_applied_constraints, "Date"] = chosen_date
+            time_restrictions.loc[n_applied_constraints, "Constraint Type"] = constraint_type
+            time_restrictions.loc[n_applied_constraints, "Constraint Time"] = constraint_time
+
+            # Updating applied_constraints dict
+            if lookup_key in applied_constraints:
+                applied_constraints[lookup_key].append((constraint_type, constraint_time))
+            else:
+                applied_constraints[lookup_key] = [(constraint_type, constraint_time)]
+
+            n_applied_constraints += 1
+
+        return time_restrictions
+
+    def _generate_venue_restrictions(self, n_restrictions):
+        pass
 
 def random_between_two_times(time_start, time_end, rounding_method="floor"):
     time_diff = time_end - time_start
@@ -130,12 +204,17 @@ if __name__ == "__main__":
     shared_coaches = 3
     venue_n_courts = [1, 2]
     date_start = datetime.datetime(2020, 3, 25)
+    n_time_constraints = 10
+    n_venue_constraints = 5
+
     constraints_generator = ConstraintsGenerator(seed)
 
     constraints_generator.generate_scenario(teams_per_division,
                           shared_coaches, game_length,
-                          venue_n_courts, date_start)
+                          venue_n_courts, date_start,
+                          n_time_constraints, n_venue_constraints)
 
     print(constraints_generator.court_availabilities)
+    print(constraints_generator.teams_time_restrictions)
 
 
