b7dc78fecb3759d877e886fed5c14734a7462db6
mikaelgelinas <mikael.gelinas@gmail.com>
2020-04-10 14:26:43 -0400

    Modification au pipeline pour ajouter les résultats

--- code/run_minizinc_batch.py
+++ code/run_minizinc_batch.py
@@ -1,11 +1,47 @@
 import subprocess
-from os.path import join, abspath
+from os.path import join, abspath, exists, dirname
+from os import makedirs
+import os
+import mmap
+
+def extractDataToCSV(outputPath):
+
+    unsatisfiable = True
+    stringtime = ""
+    stringnode = ""
+    stringnogood = ""
+    with open(outputPath) as f:
+        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        if(s.find(b'=====UNSATISFIABLE=====') != -1):
+            #print("The scenario is unsatisfiable.")
+            unsatisfiable = True
+        else:
+            unsatisfiable = False
+            f.seek(0, os.SEEK_SET)
+            for line in f.readlines():
+                time = line.find("%%%mzn-stat: time=")
+                node = line.find("%%%mzn-stat: nodes=")
+                nogood = line.find("%%%mzn-stat: nogoods=")
+                slashn = line.find("\n")
+                if(time != -1):
+                    stringtime = line[len("%%%mzn-stat: time="): slashn]
+                if(node != -1):
+                    stringnode = line[len("%%%mzn-stat: nodes="): slashn]
+                if(nogood != -1):
+                    stringnogood = line[len("%%%mzn-stat: nogoods="): slashn]
+    
+    return unsatisfiable, stringtime, stringnode, stringnogood
 
 def excuteMinizinc(pathTomodel, pathToScenario, outputPath, mode='w'):
     process = subprocess.Popen(['minizinc', '--solver', 'chuffed', '--verbose-solving', '--solver-statistics', '-f', pathTomodel, pathToScenario], 
                             stdout=subprocess.PIPE,
                             universal_newlines=True)
 
+    asbsolutpath = dirname(abspath(outputPath))
+    if not exists(asbsolutpath):
+        makedirs(asbsolutpath)
+
+    print("Saving full result to : " + abspath(outputPath))
     f = open(outputPath, mode)
     while True:
         output = process.stdout.readline()
@@ -20,9 +56,11 @@ def excuteMinizinc(pathTomodel, pathToScenario, outputPath, mode='w'):
                 print(output.strip())
                 f.writelines(str(output.strip()) + '\n')
             break
-    print("Saving result to : " + abspath(outputPath))
     f.close()
+    print("Saving full result to : " + abspath(outputPath) + ". Done.")
+
+    return extractDataToCSV(outputPath)
 
 
 if(__name__ == "__main__"):
-    excuteMinizinc(join('models', 'model2.mzn'), join('models', 'scenario 2.dzn'), join('models', 'result.txt'))
+    excuteMinizinc(join('models', 'model2.mzn'), join('models', 'scenario2.dzn'), join('test', 'result.txt'), "outputcsv.csv")
