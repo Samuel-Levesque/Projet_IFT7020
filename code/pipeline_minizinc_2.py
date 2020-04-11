e1899380c0b4e1f065583b36ba2a7225692f35d8
Bruno Kinder <almentero@gmail.com>
2020-04-11 16:05:01 -0400

    First results

--- code/pipeline_minizinc_2.py
+++ code/pipeline_minizinc_2.py
@@ -1,15 +1,20 @@
-import run_minizinc_batch as batch
+import run_minizinc_batch_2 as batch
 import dzn_generator as generator
 import dzn_export as export
 from os.path import join, exists
 
+def add2file(path, content):
+    with open(path, mode='a') as f:
+        f.writelines(content)
+
 if __name__ == "__main__":
     
-    pourcentage_nb_coach = 0.95 
+    id = "bruno"
+    pourcentage_nb_coach = 1 
     models = ["model_alldiff_regular", "model_alldiff_sat", "model_sums_regular", "model_sums_sat"]
     scenarios = []
 
-    for n_teams in (5, 40, 5):
+    for n_teams in range(5, 40, 5):
 
         scenario = {
                 "name" : "toy",
@@ -37,37 +42,30 @@ if __name__ == "__main__":
             d = s["n_teams_per_division"]
             b = s["break_duration"]
             
-            dzn_file = f"test/{n}-{p}-{v}-{t}-{c}-{d}-{b}.dzn"
-            result_file = f"test/{n}-{p}-{v}-{t}-{c}-{d}-{b}.txt"
+            test_path = f"test_{id}/{model}-t{t}-p{p}-d{len(d)}-v{v}-c{c}-{b}"
+            dzn_file = f"{test_path}.dzn"
+            result_file = f"{test_path}.txt"
 
             random_scenario = scenario.generate_scenario(n, p, v, t, c, d) 
             export.export(dzn_file, random_scenario)
           
-            outputSatisfiable = "satisfiable.out"
-            outputUnSatisfiable = "un_satisfiable.out"
-            outputUnSatisfiableTimeout = "timeout.out"
-            outputUnSatisfiableError = "error.out"
+            outputSatisfiable = f"test_{id}/satisfiable.out"
+            outputUnSatisfiable = f"test_{id}/un_satisfiable.out"
+            outputUnSatisfiableTimeout = f"test_{id}/timeout.out"
+            outputUnSatisfiableError = f"test_{id}/error.out"
 
-            resultat = f"{model};{n};{p};{v};{t};{c};{d};{b}"
-            print("Trying : " + resultat)
+            resultat = f"{model};{t};{p};{len(d)};{v};{c};{b}"
+            print("Model: " + resultat)
 
             try:
-                unsatisfiable, stringtime, stringnode, stringnogood = batch.excuteMinizinc(f"models/{model}.mzn", dzn_file, result_file, 10*60*1000)
+                unsatisfiable, duration, node, nogood = batch.excuteMinizinc(f"../models/{model}.mzn", dzn_file, result_file, 10*60*1000)
 
                 if(unsatisfiable):
                     print("The scenario is unsatisfiable.")
-                    resultat = resultat + '\n'
-                    with open(outputUnSatisfiable, mode='a') as f:
-                        f.writelines(resultat)
-                elif(not unsatisfiable and(stringtime == '' or stringnode == '' or stringnogood == '')):
-                    resultat = resultat + '\n'
-                    with open(outputUnSatisfiableError, mode='a') as f:
-                        f.writelines(resultat)
+                    add2file(outputUnSatisfiable, resultat + '\n')
+                elif(not unsatisfiable and (duration == '' or node == '' or nogood == '')):
+                    add2file(outputUnSatisfiableError, resultat + '\n')
                 else:
-                    resultat = resultat + ';' + stringtime + ';' + stringnode + ';' + stringnogood + '\n'
-                    with open(outputSatisfiable, mode='a') as f:
-                        f.writelines(resultat)
+                    add2file(outputSatisfiable, f"{resultat};{duration};{node};{nogood}\n")
             except Exception as inst:
-                resultat = resultat + ';' + str(inst.args) + '\n'
-                with open(outputUnSatisfiableTimeout, mode='a') as f:
-                        f.writelines(resultat)
+                add2file(outputUnSatisfiableTimeout, f"{resultat};{str(inst.args)}\n")
