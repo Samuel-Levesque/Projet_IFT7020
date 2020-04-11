da5e7f2e9436ca5b6c6c6920d142c3b5b4bd431a
mikaelgelinas <mikael.gelinas@gmail.com>
2020-04-11 13:55:02 -0400

    Tentative d'amélioration du process de pipeline pour aller minizinc

--- code/pipeline_minizinc.py
+++ code/pipeline_minizinc.py
@@ -1,7 +1,7 @@
 import run_minizinc_batch as batch
 import dzn_generator as generator
 import dzn_export as export
-from os.path import join
+from os.path import join, exists
 
 def n_teams_per_division(n_teams, nb_teams_by_division):
     nb_division = int(n_teams//nb_teams_by_division)
@@ -42,7 +42,8 @@ if __name__ == "__main__":
     #                 scenarios.append(createScenario(p, v, t, model))
 
     #createScenario(n_periods, n_venues, n_teams, pourcentage_nb_coach, nb_teams_by_division, model)
-    scenarios.append(createScenario(10, 10, 8, 1, 2, models[0]))
+    #scenarios.append(createScenario(40, 20, 60, 1, 5, models[1]))
+    scenarios.append(createScenario(4, 4, 8, 1, 2, models[1]))
     scenario = generator.Scenario(seed=456) 
 
     for s in scenarios:
@@ -59,35 +60,42 @@ if __name__ == "__main__":
         dzn_file = join('test', resultName + '.dzn')
 
         #print(resultName)
-        random_scenario = scenario.generate_scenario(n, p, v, t, c, d) 
-        export.export(dzn_file, random_scenario)
+        if(True):
+        #if(exists(dzn_file)):
+        #    print('Already processed : ' + dzn_file)
+        #else:
+            random_scenario = scenario.generate_scenario(n, p, v, t, c, d) 
+            export.export(dzn_file, random_scenario)
 
-        s1 = f"Q = {b+2} and replace the DFA for:\n"
-        s1 += scenario.generate_dfa(b)
-        export.create_file("dfa.mzn", s1)
+            s1 = f"Q = {b+2} and replace the DFA for:\n"
+            s1 += scenario.generate_dfa(b)
+            export.create_file("dfa.mzn", s1)
 
-        outputSatisfiable = "outputSatisfiable.csv"
-        outputUnSatisfiable = "outputUnSatisfiable.csv"
-        outputUnSatisfiableTimeout = "outputUnSatisfiableTimeout.csv"
+            outputSatisfiable = "outputSatisfiable.csv"
+            outputUnSatisfiable = "outputUnSatisfiable.csv"
+            outputUnSatisfiableTimeout = "outputUnSatisfiableTimeout.csv"
+            outputUnSatisfiableError = "outputUnSatisfiableError.csv"
 
-        resultat = s["model"] + ' ' + str(n) + ' ' + str(p) + ' ' + str(v) + ' ' + str(t) + ' ' + str(c) + ' "' + str(d) + '" ' + str(b)
-        print("Trying : " + resultat)
+            resultat = s["model"] + ';' + str(n) + ';' + str(p) + ';' + str(v) + ';' + str(t) + ';' + str(c) + ';"' + str(d) + '";' + str(b)
+            print("Trying : " + resultat)
 
-        try:
-            unsatisfiable, stringtime, stringnode, stringnogood = batch.excuteMinizinc(s["model"], dzn_file, join('test', resultName + '.txt'), 10*60)
+            try:
+                unsatisfiable, stringtime, stringnode, stringnogood = batch.excuteMinizinc(s["model"], dzn_file, join('test', resultName + '.txt'), 10000)
 
-            if(unsatisfiable):
-                print("The scenario is unsatisfiable.")
-                resultat = resultat + '\n'
-
-                with open(outputUnSatisfiable, mode='a') as f:
-                    f.writelines(resultat)
-            else:
-                resultat = resultat + ' ' + stringtime + ' ' + stringnode + ' ' + stringnogood + '\n'
-
-                with open(outputSatisfiable, mode='a') as f:
-                    f.writelines(resultat)
-        except Exception as inst:
-            resultat = resultat + ' ' + str(inst.args) + '\n'
-            with open(outputUnSatisfiableTimeout, mode='a') as f:
-                    f.writelines(resultat)
+                if(unsatisfiable):
+                    print("The scenario is unsatisfiable.")
+                    resultat = resultat + '\n'
+                    with open(outputUnSatisfiable, mode='a') as f:
+                        f.writelines(resultat)
+                elif(not unsatisfiable and(stringtime == '' or stringnode == '' or stringnogood == '')):
+                    resultat = resultat + '\n'
+                    with open(outputUnSatisfiableError, mode='a') as f:
+                        f.writelines(resultat)
+                else:
+                    resultat = resultat + ';' + stringtime + ';' + stringnode + ';' + stringnogood + '\n'
+                    with open(outputSatisfiable, mode='a') as f:
+                        f.writelines(resultat)
+            except Exception as inst:
+                resultat = resultat + ';' + str(inst.args) + '\n'
+                with open(outputUnSatisfiableTimeout, mode='a') as f:
+                        f.writelines(resultat)
